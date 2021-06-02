d1204f1451221021f4b68d258993d76d5fdde2ed
Ernesto Martinez <ernestomm99@hotmail.com>
2021-06-02 12:39:25 -0600

    Number, Boolean y String

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,43 @@
-console.log('Hola TypeScript platzi')
+console.log('Hola TypeScript platzi');
+
+//Number
+//Explicit
+let phone: number;
+phone = 1;
+phone = 54234567;
+// phone = 'hola'; //Error
+
+//Inferido
+let phoneNumber = 54234567;
+phoneNumber = 123;
+// phoneNumber = true; //Error por tipo
+
+let hex: number = 0xf00d;
+let binary: number = 0b1010;
+let octal: number = 0o744;
+
+// Tipo Boolean
+let isPro: boolean;
+isPro = true;
+//isPro = 1; //Error por tipo
+
+// Inferido
+let isUserPro = false;
+isUserPro = true;
+//isPro = 1; //Error por tipo
+
+//Tipo String
+let userName: string = 'Eru';
+// userName = true; //Error por tipo
+
+//Template string
+//uso de backtick `
+let userInfo: string;
+userInfo = `
+	User Info:
+	userName:${userName}
+	fullName:${userName + ' Martinez'}
+	phone:${phone}
+	isPro:${isPro}
+`;
+console.log('userInfo', userInfo);
