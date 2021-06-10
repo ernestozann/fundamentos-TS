5b08a70a5d303da5c90a5d5a5b8725654fd1d6c1
Ernesto Martinez <ernestomm99@hotmail.com>
2021-06-10 09:55:45 -0600

    interface:propiedades opcionales

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -33,3 +33,41 @@ showPicture({
 	orientation: PhotoOrientation.portrait,
 	// extra: 'test', //error
 })
+
+interface PictureConfig {
+	title?: string,
+	date?: string,
+	orientation?: PhotoOrientation,
+}
+
+function generatePicture(config: PictureConfig) {
+	const pic = {title: 'Default', date: '2021-06-10'};
+	if (config.title) {
+		pic.title = config.title;
+	}
+	if (config.date) {
+		pic.date = config.date;
+	}
+	return pic;
+}
+
+let picture = generatePicture({});
+console.log('picture', picture);
+picture = generatePicture({title: 'Travel Pic'});
+console.log('picture', picture);
+picture = generatePicture({title: 'Travel Pic', date: '2042-06-10'});
+console.log('picture', picture);
+
+// Interface: Users
+interface User {
+	readonly id: number,
+	username: string,
+	isPro: boolean,
+}
+
+let user: User;
+user = {id: 10, username: 'Eru-san', isPro: true};
+console.log('user:', user);
+user.username = 'Eru';
+console.log('user:', user);
+// user.id = 25; //error
